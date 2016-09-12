a1edb884ed7fd7b22e7e0a45fe940436c2a8826b
gebteus <gebteus@gmail.com>
2016-09-12 18:33:45 +0300

    Change logic

--- mongozbx.py
+++ mongozbx.py
@@ -41,6 +41,72 @@ def parse_args():
         help='query is the complete query (including command).')
     return parser.parse_args()
 
+class q_handler:
+    _instr_ = []
+
+    def __init__(self, uri, command, query):
+        if isinstance(command, str) is False or len(command) == 0:
+            raise Exception('Command invalid')
+        self.mongoclient = pymongo.MongoClient(uri)
+        if command in self._instr_:
+            self.db = self.mongoclient.get_database(name='admin')
+        else:
+            self.db = self.mongoclient.get_default_database()
+        self.command = command
+        self.query = query
+
+    def __del__(self):
+        if self.mongoclient:
+            self.mongoclient.close()
+
+    def execute(self):
+        if self.command in self._instr_:
+            glob = globals()
+            function = glob.get(self.command)
+            if function:
+                return function(self.db)
+            else:
+                raise Exception('Function \'{0}\' not found'.format(
+                    self.command))
+        else:
+            return self.db.command(
+                SON(self.prepare()))
+        return {}
+
+    def prepare(self):
+        command = self.command
+        query = self.query
+        if isinstance(query, list) is False or len(query) == 0:
+            raise Exception('Query invalid')
+        i = 0
+        while True:
+            i += 1
+            if 0 < i < len(query):
+                if '=' not in query[i]:
+                    query[i-1] += ' ' + query.pop(i)
+                    i = 0
+            else:
+                break
+        for item in query:
+            if '=' not in item:
+                raise Exception('Query is not correct')
+        query = dict(s.split('=') for s in query)
+        if command not in query:
+            raise Exception('Didn\'t find command \'{0}\' in query'.format(command))
+        qlist = []
+        qkeys = list(
+            query.keys())
+        qlist.append(
+            (command, query.get(command)))
+        del qkeys[qkeys.index(command)]
+        for key in qkeys:
+            qlist.append(
+                (key, query.get(key)))
+        return qlist
+
+def stdout(message=''):
+    sys.stdout.write('{0}\n'.format(message))
+
 def get_value_through_path(data, path):
     item = data
     try:
@@ -54,63 +120,20 @@ def get_value_through_path(data, path):
         pass
     return item
 
-def connect(uri):
-    return pymongo.MongoClient(uri)
-
-def prepare_query(command, query):
-    if not command or not isinstance(command, str):
-        raise Exception('Command invalid')
-    if not query or not isinstance(query, list):
-        raise Exception('Query invalid')
-    i = 0
-    while True:
-        i += 1
-        if 0 < i < len(query):
-            if '=' not in query[i]:
-                query[i-1] += ' ' + query.pop(i)
-                i = 0
-        else:
-            break
-    for item in query:
-        if '=' not in item:
-            raise Exception('Query is not correct')
-    query = dict(s.split('=') for s in query)
-    if command not in query:
-        raise Exception('Didn\'t find command \'{0}\' in query'.format(command))
-    qlist = []
-    qkeys = list(
-        query.keys())
-    qlist.append(
-        (command, query.get(command)))
-    del qkeys[qkeys.index(command)]
-    for key in qkeys:
-        qlist.append(
-            (key, query.get(key))) 
-    return qlist
-
-def stdout(message=''):
-    sys.stdout.write('{0}\n'.format(message))
-
-def main():
+if __name__ == '__main__':
     try:
         args = parse_args()
-        mclient = pymongo.MongoClient(args.uri)
+        hdlr = q_handler(args.uri, args.command, args.query)
         try:
-            db = mclient.get_default_database()
-            query = prepare_query(args.command, args.query)
-            response = db.command(
-                SON(query))
+            response = hdlr.execute()
             stdout(json.dumps(
                 get_value_through_path(response, args.path), default=json_util.default))
         except Exception as _err:
             raise _err
         finally:
-            mclient.close()
+            del hdlr
     except Exception as _err:
         sys.exit(
             stderr(_err))
     else:
         sys.exit(0)
-
-if __name__ == '__main__':
-    main()
